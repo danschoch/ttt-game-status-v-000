940737bd2854ac0fd3ca901a0a516a8047b1f6cf
Daniel Schoch <d.schoch90@gmail.com>
2018-08-14 00:48:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,25 +6,15 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [0,1,2]
-  [3,4,5]
-  [6,7,8]
-  [0,3,6]
-  [1,4,7]
-  [2,5,8]
-  [0,4,8]
-  [2,4,6]
+  [0,1,2], #top_row_win
+  [3,4,5], #middle_row_win
+  [6,7,8], #bottom_row_win
+  [0,3,6], #left_column_win
+  [1,4,7], #middle_column_win
+  [2,5,8], #right_column_win
+  [0,4,8], #diagonal_LtoR_win
+  [2,4,6], #diagonal_RtoL_win
 ]
-
-
-  [0,1,2] #top_row_win
-  [3,4,5] #middle_row_win
-  [6,7,8] #bottom_row_win
-  [0,3,6] #left_column_win
-  [1,4,7] #middle_column_win
-  [2,5,8] #right_column_win
-  [0,4,8] #diagonal_LtoR_win
-  [2,4,6] #diagonal_RtoL_win
 
 
 def won?
