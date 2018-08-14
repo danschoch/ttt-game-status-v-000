5e1d724fe4abda0f42567d3d36beb29eb5cc35f2
Daniel Schoch <d.schoch90@gmail.com>
2018-08-14 00:37:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,16 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2] #top_row_win
+  [3,4,5] #middle_row_win
+  [6,7.8] #bottom_row_win
+  [0,3,6] #left_column_win
+  [1,4,7] #middle_column_win
+  [2,5,8] #right_column_win
+  [0,4,8] #diagonal_LtoR_win
+  [2,4,6] #diagonal_RtoL_win
+]
+
+def won?
