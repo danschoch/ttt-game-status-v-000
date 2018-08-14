77834b5b7d5c2ad799d6f7833261dc3be26323ff
Daniel Schoch <d.schoch90@gmail.com>
2018-08-14 00:39:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ end
 WIN_COMBINATIONS = [
   [0,1,2] #top_row_win
   [3,4,5] #middle_row_win
-  [6,7.8] #bottom_row_win
+  [6,7,8] #bottom_row_win
   [0,3,6] #left_column_win
   [1,4,7] #middle_column_win
   [2,5,8] #right_column_win
